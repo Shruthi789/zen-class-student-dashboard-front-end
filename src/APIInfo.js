dbf24c232c7d6d1e76d795db2ea46a3906540cbd
Shruthi Sridhar <shruthisridhar96@gmail.com>
2022-05-01 18:13:02 +0530

    API Info Changes

--- src/APIInfo.js
+++ src/APIInfo.js
@@ -1,4 +1,4 @@
-const API='https://zen-class-student-backend.herokuapp.com/'; //Production
+const API='https://zen-class-student-backend.herokuapp.com'; //Production
 //const API='http://localhost:9000'; //Development
 
 export {API};
